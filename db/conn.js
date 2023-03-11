bf3512dab9661259eac4a40c3efe51fb533324a7
umairzahid604 <zahidumair042@gmail.com>
2023-03-11 11:16:02 +0500

    b

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-const Database = process.env.MONGO_DB_URI
+// const Database = process.env.MONGO_DB_URI
 mongoose.set("strictQuery","false")
-module.exports =  mongoose.connect(Database)
+module.exports =  mongoose.connect("mongodb+srv://Xnargy042:XNARGY12345@cluster0.i18iuei.mongodb.net/ScanedData")
 .then(() => console.log("connection succesfully set"))
 .catch((err) => console.log(err));
